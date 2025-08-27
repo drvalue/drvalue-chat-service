748eede1b62788d5072335f24a6fad3a2dd6cef3
승예지 <112852757+uhngzi@users.noreply.github.com>
2025-08-27 20:14:40 +0900

    fix: 워크 플로우 추가

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 window.__WIDGET_MANIFEST__ = {
-  version: "2025-08-27-00003",
-  url: "https://cdn.jsdelivr.net/gh/drvalue/drvalue-chat-service@main/widget.js",
+  version: "YYYYMMDD-HHMMSS",
+  url: "https://cdn.jsdelivr.net/gh/drvalue/drvalue-chat-service@<SHORT_SHA>/widget.js",
 };
