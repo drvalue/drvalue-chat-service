4762fe24f3d9e44c4385ed1a7c2ec603437c0b42
승예지 <112852757+uhngzi@users.noreply.github.com>
2025-08-27 19:42:13 +0900

    fix: 버전 수정

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 window.__WIDGET_MANIFEST__ = {
-  version: "2025-08-27-03",
-  url: "https://cdn.jsdelivr.net/gh/drvalue/drvalue-chat-service/widget.js",
+  version: "2025-08-27-02",
+  url: "https://cdn.jsdelivr.net/gh/drvalue/drvalue-chat-service@main/widget.js",
 };
