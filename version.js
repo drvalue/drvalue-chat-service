62f9f9295088f0f24e0676f7b6fe3e6424827370
승예지 <112852757+uhngzi@users.noreply.github.com>
2025-08-27 20:26:38 +0900

    chore: set version.js 20250827-112619

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 window.__WIDGET_MANIFEST__ = {
-  version: "YYYYMMDD-HHMMSS",
-  url: "https://cdn.jsdelivr.net/gh/drvalue/drvalue-chat-service@<SHORT_SHA>/widget.js",
+  version: "20250827-112619",
+  url: "https://cdn.jsdelivr.net/gh/drvalue/drvalue-chat-service@bf66a27/widget.js"
 };
